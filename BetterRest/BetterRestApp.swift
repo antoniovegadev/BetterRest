1d230916076e3cffb6c7ab2c0991dc1f202641fc
antonivegaochoa <tonivegaochoa@gmail.com>
2021-04-16 18:15:20 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Antonio Vega on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
